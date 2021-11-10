7af311fcfd9a6e25854ca3fdd8039b318012f93c
sekhparvez <sekhparvez@gmail.com>
2021-11-09 20:52:38 -0500

    done

--- src/components/App.js
+++ src/components/App.js
@@ -5,27 +5,53 @@ import Stories from './Stories';
 
 const navItems = ["arts", "books", "fashion", "food", "movies", "travel"];
 const nytapi = 'gLXaEYxlWPv5qcfDC3k61pDIQAId9wlm'
-const section = 'arts';
+// const section = 'arts';
 
 
 function App() {
   const [stories, setStories] = React.useState([]);
   const [loading, setLoading] = React.useState(false);
-
+  const [section, setSection] = React.useState('arts');
+  
   React.useEffect(() => {
+    const url = new URL(window.location.href);
+    const hash = url.hash.slice(1);
+    if (hash !== "undefined") {
+      console.log(" hash ", hash);
+      setSection(hash);
+    } else {
+      setSection("arts");
+    }
+  }, []);
+  
+  React.useEffect(() => {
+    setLoading(true);
     fetch(
       `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${nytapi}`
     )
       .then((response) => response.json())
-      .then((data) => setStories(data.results));
-  }, []);
+      .then((data) => setStories(data.results))
+      .then(setLoading(false))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [section]);
     
+  // if(loading){
+  //   return <h2>Loading...</h2>
+  // }
 
   return (
     <div>
       <Header siteTitle = "All the News that Fits We Print" />
-      <Nav navItems={navItems}></Nav>
-      <Stories stories={stories}/>
+      <Nav navItems={navItems} setSection = {setSection} section={section}></Nav>
+      {
+        loading || stories.length === 0 ? (
+          <h2>Loading...</h2>
+        ) : <Stories stories={stories} section={section} />
+
+      }
+      
     </div>
   );
 }
